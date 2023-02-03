bc5688f75f760e4869e70f9b22b2ec088dd44c40
nouhaila-m <81920166+nouhaila-m@users.noreply.github.com>
2023-02-03 18:45:06 +0100

    somechanges

--- resources/views/Accueil.blade.php
+++ resources/views/Accueil.blade.php
@@ -1,147 +1,250 @@
-<!DOCTYPE html>
-<html>
-<head>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="./style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://kit.fontawesome.com/3bce00c912.css" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/3bce00c912.js" crossorigin="anonymous"></script>
+    <title>Medecin Interface!</title>
+  </head>
+  <body>
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/style.css') }}" />
-    <link rel="shortcut icon" href="assets/images/Frame02.png"/>
-    <title>Page Title</title>
-</head>
-<body>
-  <header>
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+    <!-- As a link -->
+    <nav class="navbar " style="height: 80px !important;">
+        <a class="navbar-brand" href="#" style=" color: white;  text-decoration-line: underline  ">Logo</a>
+        <a class="navbar-brand" href="" style=" color: white;   text-decoration: underline solid #93E3A9 50%;  ">Analyse de perscription</a>
+        <a class="navbar-brand" href="#" style=" color: white;">Monographies de medicaments</a>
+        <a><i class="bi bi-person" style="font-size: 60px; color: rgb(253, 253, 252);"></i></a>
+      
+
     </nav>
-  </header>
-<div id="user"></div>
-  <div>
-    <div class="searchInput_container">
-        <div class="searchInput">
-            <input type="text" placeholder="Saisir un medicament.." id="search">
-            <div class="resultBox">
-            <!-- here list are inserted from javascript -->
+    
+    
+    <div class="p-4" >
+        <div class="row m-1 ">
+            <div class="col-8">
+            
+                <div class="card" style="height: 100% !important; ">
+                    
+                    <div class="input-group card-header border-0 bg-white">
+                        <span class="input-group-text border-0  bg-white "><i class="bi bi-search-heart"></i></i></span>
+                        <input class="form-control mr-2 rounded "  type="text" placeholder="Rechercher des medicaments" >
+                        <button class="btn bg-white border rounded"><i class="bi bi-camera"></i></button>
+                    </div>
+                    
+                    <div class="card-body">
+                        <div class="container">
+                            <p class="card-text text-center" style=" margin-top: 30vh;">Votre ordonnance Rx est vide.<br>Veuillez ajouter un médicament à partir de la barre de recherche</p>
+                        </div>
+                    </div>
+                    
+                    <div class="card-footer bg-white">
+                     <label for="inputGroupSelect01">Vos ordonnances enregistrées :</label>
+                     <div class="d-flex justify-content-between">
+                        <div class=" m-1 " style="width: 30% ;">
+                            <select class="custom-select" id="inputGroupSelect01">
+                                <option selected>Aucun</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div >
+                            <button class="btn"><i class="bi bi-printer mr-1"></i>Imprimer</button>
+                            <button class="btn"><i class="bi bi-download mr-1"></i>Sauvgarder</button>
+                            <button class="btn"><i class="bi bi-archive mr-1"></i>Degager</button>
+                        </div>
+                     </div>
+                     
+                    </div>
+
+                  </div>
             </div>
-            <div class="icon_searchInput"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-</svg></i></div>
+
+            <div class="col-4 border-0 rounded mr-0 ">
+                <div class="card" >
+                    <div class="card-header  bg-white border-0">
+                        <h5 class="text-center" style="color:#003735">PROFIL CLINIQUE</h3>
+                        <hr>
+                    </div>
+
+                    <div class="card-body container ">
+                        <div class=" d-flex justify-content-between">
+                            <div class="btn-group btn-group-toggle mr-1 " data-toggle="buttons">
+                                <label class="btn active">
+                                  <input type="radio" name="options" id="male" autocomplete="off" checked> <i class="bi bi-gender-male"></i> Male
+                                </label>
+                                <label class="btn ">
+                                  <input type="radio" name="options" id="female" autocomplete="off"><i class="bi bi-gender-female"></i> Female
+                                </label>
+                            </div>
+                            <input id="startDate" class="form-control" type="date" />
+                        </div>
+
+                        <div class="mt-2  d-flex">
+                            <div class="input-group m-1">
+                                <input type="number" class="form-control border-right-0" placeholder="Masse">
+                                <span class="input-group-text bg-white">kg</span>
+                            </div>
+                            <div class="input-group m-1">
+                                <input type="number" class="form-control border-right-0" placeholder="Hauteur">
+                                <span class="input-group-text bg-white">cm</span>
+                            </div>
+                        </div>
+
+                        <div class="mt-2 border border-light rounded-2 bg-white d-flex justify-content-between " >
+
+                            <p class="m-1"><i class="bi bi-lungs" ></i> Dommages rénaux</p>
+                            <div class=" btn-group-toggle m-1 " data-toggle="buttons">
+                                <label class="btn btn-sm active">
+                                  <input type="radio" name="options" id="oui" autocomplete="off" checked>  Non
+                                </label>
+                                <label class="btn btn-sm">
+                                  <input type="radio" name="options" id="non" autocomplete="off">    Oui
+                                </label>
+                            </div>
+
+                        </div>
+                        <div class="mt-2 border border-light rounded-2 bg-white d-flex justify-content-between " >
+
+                            <p class="m-1"><i class="bi bi-lungs"></i> Lésions hépatiques</p>
+                            <div class=" btn-group-toggle m-1 " data-toggle="buttons">
+                                <label class="btn btn-sm active">
+                                  <input type="radio" name="options" id="oui" autocomplete="off" checked>  Non
+                                </label>
+                                <label class="btn btn-sm">
+                                  <input type="radio" name="options" id="non" autocomplete="off">    Oui
+                                </label>
+                            </div>
+
+                        </div>
+                        <div class="mt-2 border border-light rounded-2 bg-white d-flex justify-content-between " >
+
+                            <p class="m-1"><i class="bi bi-lungs"></i> Dommages cardiaques</p>
+                            <div class=" btn-group-toggle m-1 " data-toggle="buttons">
+                                <label class="btn btn-sm active">
+                                  <input type="radio" name="options" id="oui" autocomplete="off" checked>  Non
+                                </label>
+                                <label class="btn btn-sm">
+                                  <input type="radio" name="options" id="non" autocomplete="off">    Oui
+                                </label>
+                            </div>
+
+                        </div>
+                        <div class="mt-2 border border-light rounded-2 bg-white d-flex justify-content-between " >
+
+                            <p class="m-1"><i class="bi bi-lungs"></i> Dommages cardiaques</p>
+                            <div class=" btn-group-toggle m-1 " data-toggle="buttons">
+                                <label class="btn btn-sm active">
+                                  <input type="radio" name="options" id="oui" autocomplete="off" checked>  Non
+                                </label>
+                                <label class="btn btn-sm">
+                                  <input type="radio" name="options" id="non" autocomplete="off">    Oui
+                                </label>
+                            </div>
+
+                        </div>
+                    
+                        <div class="mt-2 border border-light rounded-2 bg-white d-flex justify-content-between " >
+
+                            <p class="m-1"><i class="bi bi-lungs"></i> Dommages pulmonaires</p>
+                            <div class=" btn-group-toggle m-1 " data-toggle="buttons">
+                                <label class="btn btn-sm active">
+                                  <input type="radio" name="options" id="oui" autocomplete="off" checked>  Non
+                                </label>
+                                <label class="btn btn-sm">
+                                  <input type="radio" name="options" id="non" autocomplete="off">    Oui
+                                </label>
+                            </div>
+
+                        </div>
+
+                        <div class="card mt-2" >
+                            <div class="card-header">
+                                <i class="bi bi-plus-circle"> Autres Comobidités</i>
+                            </div>
+                            <div class="card-body">
+                                <div class="input-group m-1 rounded-2">
+                                    <span class="input-group-text bg-white border-right-0"><i class="bi bi-search"></i></span>
+                                    <input type="text" class="form-control  border-left-0" placeholder="Asthme, mysthénie, dialyse..."><br>
+                                </div>
+                                <div class="input-group m-1 rounded-2">
+                                    <input  type="radio" name="options" id="pas">
+                                    <span class="ml-1">Pas d’autres comorbidités</span>
+                                
+                                </div>
+                             
+                            </div>
+                        </div>
+                        <div class="card mt-2" >
+                            <div class="card-header">
+                                <i class="bi bi-plus-circle"> Allergeis médicamenteuses</i>
+                            </div>
+                            <div class="card-body">
+                                <div class="input-group m-1 rounded-2">
+                                    <span class="input-group-text bg-white border-right-0"><i class="bi bi-search"></i></span>
+                                    <input type="text" class="form-control  border-left-0" placeholder="Amoxicilline..."><br>
+                                </div>
+                                <div class="input-group m-1 rounded-2">
+                                    <input  type="radio" name="options" id="pas">
+                                    <span class="ml-1">Aucune allergie connue</span>
+                                
+                                </div>
+                             
+                            </div>
+                        </div>
+                    
+                    </div>
+                </div>
+            
+            </div>
         </div>
     </div>
- </div>
- <form action="get">
- <meta name="csrf-token" content="{{ csrf_token() }}">
- <div class="selected_meds">
-            <!-- here list are inserted from javascript -->
- </div>
- <button class="btn btn-primary verifier" type="submit">verifier</button>
- </form>
- <form action="post">
- <meta name="csrf-token" content="{{ csrf_token() }}">
-  <button class="btn btn-primary logout" type="submit">logout</button>
-</form>
-  <footer>
-    <p>Copyright &copy; 2023</p>
-  </footer>
-<script>
-                  
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
 
-    $("#search").keyup(function(e){
-    const searchInput = document.querySelector(".searchInput");
-    const resultBox = searchInput.querySelector(".resultBox");
-    const results = document.querySelector("#results");
-    const nom = e.target.value;
-    if(nom == '')  searchInput.classList.remove("active");
-    else{
-      $(resultBox).empty();
-         $.ajax({
-            type:'GET',
-            url:'/api/search/'+nom,
-            data:{nom : nom},
-             success:function(response){
-                console.log(response.medicaments);
-                if (response.medicaments.length){
-                    searchInput.classList.add("active");
-                    for (let i = 0; i < response.medicaments.length; i++){
-                      console.log(response.medicaments.length);
-                        $(resultBox).append('<li onclick="addinputmed(event)" id ="'+response.medicaments[i].nom_comercial+'">'+response.medicaments[i].nom_comercial+'('+response.medicaments[i].nom_molecule+')<li>');
-                    }
-                }
-        }
-  });
-}
-});
-$(".logout").click(function(e){
-  event.preventDefault();
-         $.ajax({
-            type:'Post',
-            url:'/api/logout',
-             success:function(response){
-              window.localStorage.setItem('user',null);
-              window.location.href = '/';
-            }
-  });
-});
-$(".verifier").click(function(e){
-  event.preventDefault();
-  var meds = $('input[name="meds"]').map(function(){ 
-                    return this.value; 
-                }).get();
-      console.log(meds);
-         $.ajax({
-            type:'get',
-            url:'/api/check',
-            data: {
-               meds :meds,
-              },
-             success:function(response){
-              console.log(response.interactions);
-            }
-  });
-});
-function addinputmed(e){
-    const searchInput = document.querySelector(".searchInput");
-    $('#search').val('');
-    searchInput.classList.remove("active");
-    const selected_meds = document.querySelector(".selected_meds");
-    const med = e.target.id;
-    $(selected_meds).append('<div class="alert mx-auto w-50 mt-3 text-center alert-success alert-block" id='+med+'><span>'+med+'</span><input type="hidden" class="med" value="'+med+'" name="meds"><div onclick="delet(event)" class="ml-1 delete">X<div></div>');
-}
-function delet(e){
-   e.target.parentElement.remove();
-}
-document.addEventListener('click', function(event) {
-  const searchInput = document.querySelector(".searchInput");
-  if (!searchInput.contains(event.target)) {
-    searchInput.classList.remove("active");
-  }
-  else if($('#search').val()){
-    searchInput.classList.add("active");
-  }
-});
-function userinit(){
-  const userspan = document.querySelector("#user");
-  var user = JSON.parse(window.localStorage.getItem('user'));
-  if(!user) {
-    window.location.replace('/login');
-  }
-  $("#user").append('<span>'+user.nom+'</span>');
-}
-window.addEventListener('load', function() {
-  userinit();
-});
-</script>
-</script>
+    <div class="container d-flex justify-content-between">
+        <button class="btn shadow-lg p-3 mb-5 bg-body rounded">Interactions</button>
+        <button class="btn shadow-lg p-3 mb-5 bg-body rounded">Etat clinique</button>
+        <button class="btn shadow-lg p-3 mb-5 bg-body rounded">Critéres STOP</button>
+        <button class="btn shadow-lg p-3 mb-5 bg-body rounded">Critéres de DEMARRAGE</button>
+        <button class="btn shadow-lg p-3 mb-5 bg-body rounded">Effets indésirables</button>
+    </div>
+
+
+
+    <div class="card text-center">
+        <div class="card-header" style="background-color: #FFFFFF;">
+       <h4 style="color:#003735;">Analyse des Interactions </h4>
+       <br>
+       <p>Remarque : les résultats que vous obtenez dépendent des informations que vous saisissez (médicaments et profil du patient)</p>
+        </div>
+        <div class="card-body">
+          
+          <p class="card-text" style="text-align:centre;"><img src="C:\Users\hp\Downloads\Icon login\Icon login\verified.png" width=10% height=10% style="margin-top: 10vh;" style="margin-bottom: 30vh;">.</p>
+          <p>Entrer au moins deux medicaments!!</p>
+          <br>
+          <p>             </p>
+          <br>
+          <p>             </p>
+          <br>
+          
+        </div>
+      </div>
+      
+
 </body>
 </html>
